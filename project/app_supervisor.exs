8560440b79d8d34570bed1dba741e430f8082513
principemestizoo <danielgonzalezsan4567@gmail.com>
2025-11-15 19:30:43 -0500

    fix: app_supersiro require file

--- project/app_supervisor.exs
+++ project/app_supervisor.exs
@@ -1,3 +1,10 @@
+Code.require_file("./team_manager.exs")
+Code.require_file("./project_manager.exs")
+Code.require_file("./mentor_manager.exs")
+Code.require_file("./chat_pubsub.exs")
+Code.require_file("./chat_room.exs")
+Code.require_file("./persistence_ets.exs")
+
 defmodule AppSupervisor do
   use Supervisor
 
